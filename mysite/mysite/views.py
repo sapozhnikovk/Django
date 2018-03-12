5cf894e9eac5e79c8d79cb7189e5ab2e8ba5c071
sapozhnikovk <sapozhnikovk@mail.ru>
2018-03-12 15:35:23 +0300

    Commit

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.template.loader import get_template
-from django.template import Template, Context
+#from django.template import Template, Context
+from django.shortcuts import render_to_response
 from django.http import Http404, HttpResponse
+from random import random
 import datetime
 
 
@@ -15,14 +17,20 @@ def main_page(request):
      <a href = "datetime">Страница Дата и время</a></body></html>'''
     return HttpResponse(main_html)
 
-
+""" Неудобный метод
 def current_datetime(request):
     today = datetime.datetime.today()
     t = get_template('current_datetime.html')
     #t = Template("<html><body>Сейчас {{ current_date }}.</body></html>")
     #html = t.render(Context({'current_date': today}))
     html = t.render({'current_date': today})
-    return HttpResponse(html)
+    return HttpResponse(html)"""
+def current_datetime(request):
+    x = random()
+    current_date = datetime.datetime.today()
+    #return render_to_response('current_datetime.html', {'current_date' : today,
+    #                                                    'x': rannum})
+    return render_to_response('current_datetime.html', locals())
 
 
 def hours_ahead(request, offset):
@@ -32,5 +40,4 @@ def hours_ahead(request, offset):
         raise Http404()
     dt = datetime.datetime.today() + datetime.timedelta(hours=offset)
     # assert False
-    html = "<html><body>Через %s часов будет %s.</body></html>" % (offset, dt)
-    return HttpResponse(html)
+    return render_to_response('hours_ahead.html', locals())
